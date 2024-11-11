61997f4e1bb70a905ddb875bcc346cbd2aeac8c2
SuYiming <129836093+BnuSuym@users.noreply.github.com>
2024-11-11 16:30:11 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -16,4 +16,4 @@ def adjust_lr(optimizer, init_lr, epoch, decay_rate=0.1, decay_epoch=30):
     for param_group in optimizer.param_groups:
         param_group['lr'] = decay*init_lr
         lr=param_group['lr']
-    return lr
+    return lr 
